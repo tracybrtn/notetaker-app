879b8badff15b043c516a61b23f6ca9a8e815e0a
tracybrtn <tracynburton@gmail.com>
2022-10-19 18:14:04 -0500

    push to Heroku - working get and post routes

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,22 +1,25 @@
 //import functions and data
 const router = require('express').Router();
-const notes = require('../../db/db.json');
+const noteDb = require('../../db/db.json');
 const { createNote } = require('../../lib/notes');
 
 // get function imports db.json data
 router.get('/notes', (req, res) => {
-    let results = notes;
+    let results = noteDb;
     res.json(results);
-})
+});
 
-// post function that writes into db.json file
+//post function that writes into db.json file
 router.post('/notes', (req, res) => {
-    let note = createNote(req.body, notes);
+    let note = createNote(req.body, noteDb);
     res.json(note);
-})
+});
 
-// router.delete('/notes/:id', (req, res) => {
+//delete function 
+//router.delete('/notes/:id', (req, res) => {
+    //should receive a query parameter containing the id of a note to delete
+    // you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file
 
-// })
+//})
 
 module.exports = router;
