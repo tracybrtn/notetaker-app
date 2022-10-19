6f364100273b3543699501fea4a46e375677fd20
tracybrtn <tracynburton@gmail.com>
2022-10-19 15:16:19 -0500

    working noteRoutes

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,6 +1,21 @@
+//include packages
 const path = require('path');
-const router = require('express').Router;
+const router = require('express').Router();
 
-//Connect server with HTML
+//CONNECT SERVER WITH HTML
+//Landing page  
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
+})
+
+//Notes page
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/notes.html'));
+})
+
+//anything else should return to the landing page
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
+})
 
 module.exports = router;
