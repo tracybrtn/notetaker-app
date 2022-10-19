d40f5101eeaf88e66b5ffa2acba5c45aeb84bcfa
tracybrtn <tracynburton@gmail.com>
2022-10-19 18:39:17 -0500

    working delete route, fixed html bug

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,7 +1,7 @@
 //import functions and data
 const router = require('express').Router();
 const noteDb = require('../../db/db.json');
-const { createNote } = require('../../lib/notes');
+const { createNote, deleteNote } = require('../../lib/notes');
 
 // get function imports db.json data
 router.get('/notes', (req, res) => {
@@ -15,11 +15,9 @@ router.post('/notes', (req, res) => {
     res.json(note);
 });
 
-//delete function 
-//router.delete('/notes/:id', (req, res) => {
-    //should receive a query parameter containing the id of a note to delete
-    // you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file
-
-//})
+router.delete('/notes/:id', (req, res) => {
+    deleteNote(noteDb, req.params.id);
+    res.json(noteDb)
+})
 
 module.exports = router;
