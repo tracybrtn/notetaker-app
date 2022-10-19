0b43a2cdc60e3a58dc133c7df8ae087a0fa0db31
tracybrtn <tracynburton@gmail.com>
2022-10-19 14:59:19 -0500

    Add Heroku

---
+++ routes/apiRoutes/noteRoutes.js
@@ -0,0 +1,5 @@
+const router = require('express').Router();
+
+//import functions and data
+
+module.exports = router;
---
+++ routes/htmlRoutes/index.js
@@ -0,0 +1,6 @@
+const path = require('path');
+const router = require('express').Router;
+
+//Connect server with HTML
+
+module.exports = router;
